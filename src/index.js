b91ad00f6407ddc24e469f4493733ea3f68fd8e8
EmeraldPangolin <meyerdeavan29205@gmail.com>
2023-04-25 23:02:06 -0500

    more fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './Router';
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
 
 import { Amplify } from 'aws-amplify'
 import config from './aws-exports'
